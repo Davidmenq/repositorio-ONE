24693d5d7b158e5248dbf734d8429a229b6bbce2
Davidmenq <davidmenq@hotmail.com>
2023-05-07 11:39:47 -0500

    reemplazando caracteres

--- encriptador.js
+++ encriptador.js
@@ -1,8 +1,22 @@
 let ingresar = document.querySelector(".ingresarTexto");
 let escribir = document.querySelector(".mensaje-encriptado");
+let codigos = {
+    "a" : "ai",
+    "e" : "",
+    "i" : "",
+    "o" : "",
+    "u" : ""
+}
+
 
 function mostrar(){
-    escribir.value=ingresar.value;
+    const letra = codigos.a;
+    //escribir.value=ingresar.value;
+    escribir.value=ingresar.value.replaceAll("a",letra);
     console.log(escribir.value);
 }
 
+function encriptar(textoIngresado){
+
+    return prueba;
+}
