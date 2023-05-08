c1e1ec55f8b30bda805670a817645ac490f8df77
Davidmenq <davidmenq@hotmail.com>
2023-05-08 15:45:28 -0500

    aparecer y desaparecer elementos segun el estado de la pagina completado

--- encriptador.js
+++ encriptador.js
@@ -1,5 +1,9 @@
 let ingresarTexto = document.querySelector(".ingresarTexto");
 let escribirTexto = document.querySelector(".mensaje-encriptado");
+let imagenEspera=document.querySelector(".imagenEspera");
+let alertaEspera1=document.querySelector(".alerta1");
+let alertaEspera2=document.querySelector(".alerta2");
+let botonCopiar=document.querySelector(".areaBoton3");
 let codigos = {
     "a" : "ai",
     "e" : "enter",
@@ -15,6 +19,10 @@ let codigosInversos = {
     "ufat" : "u"
 }
 
+//inicializando ocultos boton y textarea del encriptador
+botonCopiar.style.display="none";
+escribirTexto.style.display="none";
+
 function encriptar(){
     let almacenTexto=ingresarTexto.value;
     almacenTexto = almacenTexto.replaceAll("e",codigos.e);
@@ -23,6 +31,7 @@ function encriptar(){
     almacenTexto = almacenTexto.replaceAll("o",codigos.o);
     almacenTexto = almacenTexto.replaceAll("u",codigos.u);
     escribirTexto.value=almacenTexto;
+    ventanaActiva();
 }
 
 function desencriptar(){
@@ -33,12 +42,32 @@ function desencriptar(){
     almacenTexto = almacenTexto.replaceAll("ober",codigosInversos.ober);
     almacenTexto = almacenTexto.replaceAll("ufat",codigosInversos.ufat);
     escribirTexto.value=almacenTexto;
+    ventanaActiva();
 }
 
 function copiarTexto(){
     navigator.clipboard.writeText(escribirTexto.value);
-    ingresarTexto.value="";
-    escribirTexto.value="";
+
+    ventanaEsperando();
 }
 
+function ventanaActiva(){
+    imagenEspera.style.display="none";
+    alertaEspera1.style.display="none";
+    alertaEspera2.style.display="none";
+    botonCopiar.style.display="flex";
+    escribirTexto.style.display="block";
 
+    console.log("hola amigo ya activaste la ventana");
+}
+function ventanaEsperando(){
+    imagenEspera.style.display="block";
+    alertaEspera1.style.display="block";
+    alertaEspera2.style.display="block";
+    botonCopiar.style.display="none";
+    escribirTexto.style.display="none";
+
+    ingresarTexto.value="";
+    escribirTexto.value="";
+    console.log("Estoy en espera");
+}
